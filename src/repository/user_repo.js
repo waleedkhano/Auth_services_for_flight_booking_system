70a43ef3da8bedfcea20de61d69d56076df29c28
waleedkhano <khanow99@gmail.com>
2025-12-19 21:31:27 +0500

    added get user by id in repo layer

--- src/repository/user_repo.js
+++ src/repository/user_repo.js
@@ -1,28 +1,40 @@
-const { User } = require('../models/index');
+const { User } = require("../models/index");
 
 class UserRepository {
-    async create(data) {
-        try {
-            const user = await User.create(data);
-            return user;
-        } catch (error) {
-            console.log("Something went wrong in the repository layer");
-            throw error;
-        }
+  async create(data) {
+    try {
+      const user = await User.create(data);
+      return user;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw error;
     }
+  }
 
-    async destory(userId) {
-        try {
-            await User.destroy({
-                where: {
-                    id: userId
-                }
-            });
-        } catch (error) {
-            console.log("Something went wrong in the repository layer");
-            throw error;
-        }
+  async destory(userId) {
+    try {
+      await User.destroy({
+        where: {
+          id: userId,
+        },
+      });
+    } catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw error;
     }
+  }
+
+  async getById(userId) {
+    try {
+      const user = await User.findByPk(userId, {
+        arguments: ["id", "email"],
+      });
+      return user;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw error;
+    }
+  }
 }
 
 module.exports = UserRepository;

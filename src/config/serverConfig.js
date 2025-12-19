8805548d55b861c79495d77e16452ffe016101b5
waleedkhano <khanow99@gmail.com>
2025-12-19 21:53:06 +0500

    jwt create and verify logic

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -4,5 +4,6 @@ dotenv.config();
 
 module.exports = {
   PORT: process.env.PORT || 3001,
+  JWT_KEY: process.env.JWT_KEY,
   SALT: 10,
 };
--- src/services/user_service.js
+++ src/services/user_service.js
@@ -1,4 +1,7 @@
+const jwt = require('jsonwebtoken');
+
 const UserRepository = require('../repository/user_repo');
+const { JWT_KEY } = require('../config/serverConfig');
 
 class UserService {
     constructor() {
@@ -11,6 +14,26 @@ class UserService {
             return user;
         } catch (error) {
             console.log("Something went wrong in the service layer");
+            throw error;
+        }
+    }
+
+    createToken(user){
+        try {
+            const result = jwt.sign(user, JWT_KEY, { expiresIn: '1d' });
+            return result;
+        } catch (error) {
+            console.log("Something went wrong in the service layer");
+            throw error;
+        }
+    }
+
+    verifyToken(token){
+        try {
+            const response = jwt.verify(token, JWT_KEY);
+            return response;
+        } catch (error) {
+            console.log("Something went wrong in token validation in service layer");
             throw error;
         }
     }

7bb24be25acdb8e21d3fc52f76052defbb2b197d
waleedkhano <khanow99@gmail.com>
2025-12-19 23:26:13 +0500

    added token gen logic sigin

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -1,30 +1,54 @@
-const UserService = require('../services/user_service');
+const UserService = require("../services/user_service");
 
 const userService = new UserService();
 
 const create = async (req, res) => {
-    try {
-        const response = await userService.create({
-            email: req.body.email,
-            password: req.body.password,
-        })
-        return res.status(201).json({
-            message: "successfully created a user",
-            data: response,
-            success: true,
-            err: {}
-        })
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            message: "something went wrong",
-            data: {},
-            success: false,
-            err: error
-        })
-    }
-}
+  try {
+    const response = await userService.create({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    return res.status(201).json({
+      message: "successfully created a user",
+      data: response,
+      success: true,
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "something went wrong",
+      data: {},
+      success: false,
+      err: error,
+    });
+  }
+};
+
+const signIn = async (req, res) => {
+  try {
+    const response = await userService.signIn(
+      req.body.email,
+      req.body.password
+    );
+
+    return res.status(200).json({
+      message: "successfully signed in",
+      data: response,
+      success: true,
+      err: {},
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "something went wrong",
+      data: {},
+      success: false,
+      err: error,
+    });
+  }
+};
 
 module.exports = {
-    create
-}
+  create,
+  signIn,
+};
--- src/services/user_service.js
+++ src/services/user_service.js
@@ -1,52 +1,74 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-const UserRepository = require('../repository/user_repo');
-const { JWT_KEY } = require('../config/serverConfig');
+const UserRepository = require("../repository/user_repo");
+const { JWT_KEY } = require("../config/serverConfig");
 
 class UserService {
-    constructor() {
-        this.userRepository = new UserRepository();
-    }
+  constructor() {
+    this.userRepository = new UserRepository();
+  }
 
-    async create(data){
-        try {
-            const user = await this.userRepository.create(data);
-            return user;
-        } catch (error) {
-            console.log("Something went wrong in the service layer");
-            throw error;
+  async create(data) {
+    try {
+      const user = await this.userRepository.create(data);
+      return user;
+    } catch (error) {
+      console.log("Something went wrong in the service layer");
+      throw error;
+    }
+  }
+
+  async signIn(email, plainPassword) {
+    try {
+        //fetch user using email
+        const user = await this.userRepository.getByEmail(email);
+
+        //compare plain password with encrypted password
+        const passwordMatch = this.checkpassord(plainPassword, user.password);
+        if(!passwordMatch){
+            console.log("Password does not match");
+            throw {error: "Incorrect password"};
         }
-    }
 
-    createToken(user){
-        try {
-            const result = jwt.sign(user, JWT_KEY, { expiresIn: '1d' });
-            return result;
-        } catch (error) {
-            console.log("Something went wrong in the service layer");
-            throw error;
-        }
-    }
+        //create a JWT token and send it to the user
+        const jwtToken = this.createToken({email: user.email, id: user.id});
+        return jwtToken;
 
-    verifyToken(token){
-        try {
-            const response = jwt.verify(token, JWT_KEY);
-            return response;
-        } catch (error) {
-            console.log("Something went wrong in token validation in service layer");
-            throw error;
-        }
+    } catch (error) {
+      console.log("Something went wrong in the service layer");
+      throw error;
     }
+  }
 
-    checkpassord(userInputPlainPassword, encriptedPassword){
-        try {
-            return bcrypt.compareSync(userInputPlainPassword, encriptedPassword);
-        } catch (error) {
-            console.log("Something went wrong in password checking in service layer");
-            throw error;
-        }   
+  createToken(user) {
+    try {
+      const result = jwt.sign(user, JWT_KEY, { expiresIn: "1d" });
+      return result;
+    } catch (error) {
+      console.log("Something went wrong in the service layer");
+      throw error;
     }
+  }
+
+  verifyToken(token) {
+    try {
+      const response = jwt.verify(token, JWT_KEY);
+      return response;
+    } catch (error) {
+      console.log("Something went wrong in token validation in service layer");
+      throw error;
+    }
+  }
+
+  checkpassord(userInputPlainPassword, encriptedPassword) {
+    try {
+      return bcrypt.compareSync(userInputPlainPassword, encriptedPassword);
+    } catch (error) {
+      console.log("Something went wrong in password checking in service layer");
+      throw error;
+    }
+  }
 }
 
 module.exports = UserService;
